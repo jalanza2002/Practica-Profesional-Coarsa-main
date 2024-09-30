1607aee7845c5735397eb840628b4c89f540a782
josue <josuesaemontero@icloud.com>
2024-09-30 10:27:56 -0600

    todos los estilos se pasaron a una solo archivo de css

--- Quienes Somos.php
+++ Quienes Somos.php
@@ -6,55 +6,7 @@
     <title>Coarsa</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-    <style>
-              /* Estilo general del cuerpo */
-              body {
-            font-family: Arial, sans-serif;
-            background-color: rgb(255, 255, 255);
-            margin: 0;
-            padding: 0;
-        }
-
-        /* Encabezado */
-        .header {
-            background-color: #ffffff;
-            padding: 20px 0;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            color: rgb(0, 0, 0);
-            position: fixed; /* Fija el header en la parte superior */
-            top: 0;
-            left: 0;
-            right: 0;
-            z-index: 1000; /* Hace que el header esté por encima de otros elementos */
-        }
-
-        .header h1 {
-            margin: 0;
-            padding-left: 20px;
-        }
-
-        .nav-buttons {
-            padding-right: 20px;
-        }
-
-        .nav-buttons a {
-            text-decoration: none;
-            color: rgb(0, 0, 0);
-            background-color: #ffffff;
-            padding: 10px 20px;
-            margin-left: 10px;
-            border-radius: 5px;
-            font-size: 20px;
-            transition: background-color 0.3s;
-        }
-
-        .nav-buttons a:hover {
-            background-color: #0060A9;
-        }
-</style>    
+    <script src='main.js'></script>   
 </head>
 <body>
   <div class="header">
