4a6bb13ebbcd142d2052e8c42a55e044d91001c8
josue <josuesaemontero@icloud.com>
2024-10-03 10:43:44 -0600

    Se crea los menus de Recusros humanos y empleado para una presentacion más profesional a la hora de loguearse

---
+++ Menu Empleado.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Bienvenido Empleado</title>
+</head>
+<body>
+<div class="header">
+    <h1>Coarsa</h1>
+    <div class="nav-buttons">
+        <a href="SolicitudesEmpleado.php">Crear una Solicitud</a>
+        <a href="Consultar solicitudes.php">Consultar Solicitudes</a>
+        <a href="Log In.php">Salir</a>
+    </div>
+</div>
+</body>
+</html>
---
+++ Menu RH.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Bienvenido Recursos Humanos</title>
+</head>
+<body>
+<div class="header">
+    <h1>Coarsa</h1>
+    <div class="nav-buttons">
+        <a href="PaginaRH.php">Crear Un Puesto</a>
+        <a href="Actualizar Solicitudes.php">Revision de Solicitudes</a>
+        <a href="CrearUsuario.php">Crear Usuario</a>
+        <a href="Log In.php">Salir</a> 
+    </div>
+</div>
+</body>
+</html>
